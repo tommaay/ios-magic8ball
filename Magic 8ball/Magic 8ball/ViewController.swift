9ca953343905e93be23cd9c3c0568f31e39a0982
ToMMaaY <thuynh629@gmail.com>
2019-05-09 20:57:06 -0700

    compelted 8ball app

--- Magic 8ball/Magic 8ball/ViewController.swift
+++ Magic 8ball/Magic 8ball/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber = 0
+    
+    // interface builder outlet
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // interface builder action
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    // action to take when app loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        newBallImage()
     }
-
-
+    
+    // randomize image function
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0 ... 4)
+        
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    // shake method
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
 }
 
