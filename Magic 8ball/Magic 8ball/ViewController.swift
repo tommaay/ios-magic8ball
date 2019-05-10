1a64ba50897fdd509f44d3dfc102f5820df90d81
ToMMaaY <thuynh629@gmail.com>
2019-05-09 20:19:10 -0700

    initialized project

---
+++ Magic 8ball/Magic 8ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8ball
+//
+//  Created by Tommy Huynh on 5/9/19.
+//  Copyright © 2019 Tommy Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
